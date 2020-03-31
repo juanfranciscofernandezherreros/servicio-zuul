162916933fc38d4592e4a45782f6574b5f9c1a85
Juan Francisco Fernandez <jffernandez_atsistemas@bankinter.com>
2020-03-31 13:45:39 +0200

    feat : add configuration

--- src/main/java/com/fernandez/eureka/config/ResourceServerConfig.java
+++ src/main/java/com/fernandez/eureka/config/ResourceServerConfig.java
@@ -1,7 +1,11 @@
 package com.fernandez.eureka.config;
 
+import java.util.Arrays;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -9,6 +13,10 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableResourceServer
@@ -22,10 +30,13 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers("/api/security/oauth/**").permitAll()
+				.antMatchers(HttpMethod.GET , "/api/images/v1/section/1" , "/api/images/v1/section/1/image/**").permitAll()
+				.antMatchers(HttpMethod.GET , "/api/languages/v1","/api/languages/v1/**").permitAll()
 				.antMatchers(HttpMethod.GET, "/api/distritos","/api/barrios","/api/usuarios/usuarios").permitAll()
 				.antMatchers(HttpMethod.GET, "/api/distritos/{id}", "/api/barrios/{id}","/api/usuarios/usuarios/{id}").permitAll()
-				.antMatchers(HttpMethod.PUT,"/api/distritos/{id}").hasRole("ADMIN")
-				.anyRequest().authenticated();
+				.antMatchers(HttpMethod.PUT,"/api/distritos/{id}","/api/languages/v1").hasRole("ADMIN")
+				.anyRequest().authenticated()
+				.and().cors().configurationSource(corsConfigurationSource());
 	}
 
 	@Bean
@@ -38,6 +49,27 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
 		tokenConverter.setSigningKey("algun_codigo_secreto");
 		return tokenConverter;
+	}
+	
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration corsConfig = new CorsConfiguration();
+		corsConfig.setAllowedOrigins(Arrays.asList("*"));
+		corsConfig.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
+		corsConfig.setAllowCredentials(true);
+		corsConfig.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+		
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", corsConfig);
+		
+		return source;
+	}
+	
+	@Bean
+	public FilterRegistrationBean<CorsFilter> corsFilter(){
+		FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<CorsFilter>(new CorsFilter(corsConfigurationSource()));
+		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return bean;
 	}
 
 }
